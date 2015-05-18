2eb42e629999566b2b9fd5bff21e80f7a22ebff3
pangfulli <pflnh@me.com>
2015-05-19 00:22:33 +0800

    提交

--- DropBoxDemo/DropBoxDemo/ViewController.m
+++ DropBoxDemo/DropBoxDemo/ViewController.m
@@ -9,29 +9,51 @@
 #import "ViewController.h"
 #import <DropboxSDK/DropboxSDK.h>
 
-@interface ViewController ()<DBRestClientDelegate>
+#define tinkColor [UIColor colorWithRed:0.980f green:0.604f blue:0.000f alpha:1.00f]
+
+@interface ViewController ()<DBRestClientDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic, strong) UILabel *bbiConnect;
 @property (nonatomic, strong) DBRestClient *dbRestClient;
+@property (nonatomic, strong) NSString *path;
+@property (nonatomic, assign) NSInteger uploadIndex;
+@property (nonatomic, strong) UIProgressView *progressBar;
+@property (nonatomic, strong) UITableView *myTableView;
+@property (nonatomic, strong) UIImage *theImage;
 @end
 
 @implementation ViewController
-@synthesize bbiConnect,dbRestClient;
+@synthesize bbiConnect,dbRestClient,path,uploadIndex,progressBar,myTableView,theImage;
 - (void)viewDidLoad {
     [super viewDidLoad];
     
 
     UIBarButtonItem *item1 = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:(UIBarButtonSystemItemAction) target:self action:@selector(performAction)];
+    item1.tintColor = tinkColor;
     UIBarButtonItem *item2 = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:(UIBarButtonSystemItemRefresh) target:self action:@selector(connectToDropBox)];
+    item2.tintColor = tinkColor;
     self.navigationItem.rightBarButtonItems = @[item1,item2];;
-    
     
     NSDictionary *dic = @{NSFontAttributeName: [UIFont systemFontOfSize:15],NSForegroundColorAttributeName:[UIColor colorWithRed:0.980f green:0.604f blue:0.000f alpha:1.00f]};
     
     bbiConnect = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 100, 40)];
-    
+    bbiConnect.textColor = tinkColor;
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:bbiConnect];
-    [self.navigationItem.rightBarButtonItem setTitleTextAttributes:dic forState:(UIControlStateNormal)];
-    [self.navigationItem.leftBarButtonItem setTitleTextAttributes:dic forState:(UIControlStateNormal)];
+    [item2 setTitleTextAttributes:dic forState:(UIControlStateNormal)];
+    [item1 setTitleTextAttributes:dic forState:(UIControlStateNormal)];
+    
+    progressBar = [[UIProgressView alloc]initWithProgressViewStyle:UIProgressViewStyleBar];
+    progressBar.frame = CGRectMake(10, 0, self.view.frame.size.width-20, 10);
+    progressBar.center = CGPointMake(self.view.center.x, self.view.frame.size.height - 20);
+    progressBar.tintColor = tinkColor;
+    progressBar.trackTintColor = [UIColor greenColor];
+    [self.view addSubview:progressBar];
+    
+    myTableView = [[UITableView alloc]initWithFrame:self.view.bounds style:(UITableViewStylePlain)];
+    myTableView.delegate = self;
+    myTableView.dataSource = self;
+    [self.view addSubview:myTableView];
+    [self.view bringSubviewToFront:progressBar];
+    
     
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(handleDidLinkNotification:) name:@"didLinkToDropboxAccountNotification" object:nil];
     
@@ -53,10 +75,35 @@
     }
     
     UIAlertController *actionSheet = [UIAlertController alertControllerWithTitle:@"Upload file" message:@"select file to upload" preferredStyle:UIAlertControllerStyleActionSheet];
-    UIAlertAction *uploadFieldAction = [UIAlertAction actionWithTitle:@"Upload text file" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction *action) {}];
-    UIAlertAction *uploadImageFileAction = [UIAlertAction actionWithTitle:@"Upload image" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction *action) {}];
+    UIAlertAction *uploadTextFileAction = [UIAlertAction actionWithTitle:@"Upload text file" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction *action) {
+        
+        NSString *sourePath = [[NSBundle mainBundle] pathForResource:@"testText" ofType:@"txt"];
+        NSString *fileName = @"testText.txt";
+        NSString *destinationPath = @"/";
+        
+        [self showProgressBar];
+        
+        uploadIndex++;
+        
+        [dbRestClient uploadFile:fileName toPath:destinationPath withParentRev:nil fromPath:sourePath];
+        
+    }];
+    UIAlertAction *uploadImageFileAction = [UIAlertAction actionWithTitle:@"Upload image" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction *action) {
+        
+        [[NSOperationQueue mainQueue]addOperationWithBlock:^{
+            UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+            imagePicker.delegate = self;
+            imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+            [self.navigationController presentViewController:imagePicker animated:YES completion:nil];
+        }];
+        
+//        NSString *sourePath = [[NSBundle mainBundle] pathForResource:@"mm" ofType:@"jpg"];
+//        NSString *fileName = @"mm.jpg";
+//        [dbRestClient uploadFile:fileName toPath:@"/" withParentRev:nil fromPath:sourePath];
+        
+    }];
     UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction *action) {}];
-    [actionSheet addAction:uploadFieldAction];
+    [actionSheet addAction:uploadTextFileAction];
     [actionSheet addAction:uploadImageFileAction];
     [actionSheet addAction:cancelAction];
     [self.navigationController presentViewController:actionSheet animated:YES completion:nil];
@@ -89,7 +136,122 @@
     bbiConnect.text = @"Disconnect";
 }
 
+
+#pragma mark UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return uploadIndex;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *identifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:(UITableViewCellStyleDefault) reuseIdentifier:identifier];
+    }
+    
+    if (!theImage) {
+        theImage = [UIImage imageNamed:@"mm.jpg"];
+    }
+
+    cell.textLabel.text = [NSString stringWithFormat:@"upload%ld",uploadIndex];
+    cell.imageView.image = theImage;
+
+    return cell;
+}
+
+#pragma mark UITableViewDelegate
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return  50;
+}
+
+
+#pragma mark DBRestClientDelegate
+- (void)restClient:(DBRestClient *)client uploadFileFailedWithError:(NSError *)error
+{
+    NSLog(@"error:%@",error.localizedDescription);
+    progressBar.hidden = YES;
+}
+
+- (void)restClient:(DBRestClient *)client uploadedFile:(NSString *)destPath from:(NSString *)srcPath metadata:(DBMetadata *)metadata
+{
+    NSLog(@"metadata:%@",metadata.path);
+    
+    if (path) {
+        NSError *error;
+        NSFileManager *manager = [NSFileManager defaultManager];
+        if ([manager fileExistsAtPath:path]) {
+            [manager removeItemAtPath:path error:&error];
+            if (error) {
+                
+                [[[UIAlertView alloc]initWithTitle:@"提示" message:error.localizedDescription delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil] show];
+            }
+            else
+            {
+                [[[UIAlertView alloc]initWithTitle:@"提示" message:@"上传成功,本地删除成功" delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil] show];
+                progressBar.hidden = YES;
+            }
+        }
+    }
+    
+}
+
+- (void)restClient:(DBRestClient *)client uploadProgress:(CGFloat)progress forFile:(NSString *)destPath from:(NSString *)srcPath
+{
+    progressBar.progress = progress;
+}
+
+
+#pragma mark UIImagePickerControllerDelegate
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    theImage = image;
+    
+    [self saveImageLocally:image];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)saveImageLocally:(UIImage*)image
+{
+    NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+    path = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)[0];
+    path = [path stringByAppendingPathComponent:@"img"];
+    [imageData writeToFile:path atomically:YES];
+    
+    NSString *sourePath = path;
+    NSString *fileName =[NSString stringWithFormat:@"img%ld.jpg",uploadIndex++];
+    NSString *destinationPath = @"/";
+    [myTableView reloadData];
+    [self showProgressBar];
+    
+    [dbRestClient uploadFile:fileName toPath:destinationPath withParentRev:nil fromPath:sourePath];
+}
+
+- (void)showProgressBar
+{
+    progressBar.progress = 0.0;
+    progressBar.hidden = NO;
+}
+
 @end
+
+
+
+
+
+
 
 
 
