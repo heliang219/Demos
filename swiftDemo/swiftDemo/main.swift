a075be4f368c4b6268fdad673dac1025334f400f
pfl <pflnh@163.com>
2015-05-20 18:32:47 +0800

    no message

--- swiftDemo/swiftDemo/main.swift
+++ swiftDemo/swiftDemo/main.swift
@@ -10,3 +10,85 @@ import Foundation
 
 println("Hello, World!")
 
+//class ClassA {
+//    let numA: Int
+//    
+//   required init(num: Int)
+//    {
+//        numA = num//3
+//    }
+//    
+//    convenience required init(bigNum: Bool)
+//    {
+//        self.init(num: bigNum ? 1000 : 1)//1
+//        println(self)//5
+//    }
+//    
+//    func prin()
+//    {
+//        println(numA)
+//    }
+//    
+//    
+//}
+//
+//
+//class ClassB: ClassA {
+//    let numB: Int
+//     required init(num: Int) {
+//        
+//        // MARK: 如果numB是未初始化必须注意下面两句代码位置
+//        
+//        numB = num + 2//4
+//        super.init(num: num)//2
+//        
+//    }
+//    
+//    func print()
+//    {
+//        
+//        println(numB)
+//    }
+//    
+//    
+//}
+//
+//
+//let objB = ClassB(num: 10)
+//    objB.prin()
+//    objB.print()
+
+
+
+class A {
+    let b: B
+    init() {
+    b = B()
+    b.a = self }
+    deinit {
+    println("A deinit")
+    } }
+class B {
+   weak var a: A? = nil
+    deinit {
+    println("B deinit")
+    } }
+
+
+var obj: A? = A()
+
+    obj = nil
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 存取方法Demo/存取方法Demo/main.m
+++ 存取方法Demo/存取方法Demo/main.m
@@ -8,10 +8,105 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+#import <Foundation/Foundation.h>
+
+enum {
+    yes = 1,
+    no = 0
+} UseItTheRightWay;
+
+@interface MONObjectA : NSObject
+{
+    NSMutableArray * array;
+}
+
+@property (nonatomic, retain) NSArray * array;
+
+@end
+
+@implementation MONObjectA
+
+@synthesize array;
+
+- (id)init
+{
+    self = [super init];
+    if (0 != self) {
+        NSLog(@"%s, %@",__PRETTY_FUNCTION__, self);
+        if (UseItTheRightWay) {
+            array = [NSMutableArray new];
+        }
+        else {
+            self.array = [NSMutableArray array];
+        }
     }
+    return self;
+}
+
+- (void)dealloc
+{
+    NSLog(@"%s, %@",__PRETTY_FUNCTION__, self);
+    if (UseItTheRightWay) {
+        [array release], array = nil;
+    }
+    else {
+        self.array = nil;
+    }
+    [super dealloc];
+}
+
+@end
+
+@interface MONObjectB : MONObjectA
+{
+    NSMutableSet * set;
+}
+
+@end
+
+@implementation MONObjectB
+
+- (id)init
+{
+    self = [super init];
+    if (0 != self) {
+        NSLog(@"%s, %@",__PRETTY_FUNCTION__, self);
+        set = [NSMutableSet new];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    NSLog(@"%s, %@",__PRETTY_FUNCTION__, self);
+    [set release], set = nil;
+    [super dealloc];
+}
+
+- (void)setArray:(NSArray *)arg
+{
+    NSLog(@"%s, %@",__PRETTY_FUNCTION__, self);
+    NSMutableSet * tmp = arg ? [[NSMutableSet alloc] initWithArray:arg] : nil;
+    [super setArray:arg];
+    [set release];
+    set = tmp;
+}
+
+@end
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    [[MONObjectB new] release];
+    
+    /* the tool must be named 'Props' for this to work as expected, or you can just change 'Props' to the executable's name */
+    system("sudo leaks Props");
+    
+    [pool drain];
     return 0;
 }
+
+
+
+
+
