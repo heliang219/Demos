a0c1968f84678d9f4b7a0c67d37d5ecb6e09e28b
pfl <pflnh@163.com>
2015-05-20 18:32:24 +0800

    tijao

---
+++ swiftDemo/swiftDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftDemo
+//
+//  Created by pfl on 15/5/20.
+//  Copyright (c) 2015年 pfl. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
---
+++ 存取方法Demo/存取方法Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  存取方法Demo
+//
+//  Created by pfl on 15/5/20.
+//  Copyright (c) 2015年 pfl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
